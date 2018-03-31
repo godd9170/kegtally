f5db9474abb70907e003eca25944b3654f0b3c4b
Henry Goddard <godd9170@mylaurier.ca>
2018-03-31 14:24:01 -0400

    relative to root...nope

--- kegtally/manage.py
+++ kegtally/manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                          "kegtally.kegtally.settings")
+                          "kegtally.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
