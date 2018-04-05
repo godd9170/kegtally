6ed8a900027e44c7f040edf08caa1505799873db
Henry Goddard <godd9170@mylaurier.ca>
2018-04-04 20:10:33 -0400

    query a single keg by tag

--- inventory/schema.py
+++ inventory/schema.py
@@ -3,17 +3,22 @@ from graphene_django import DjangoObjectType
 from graphql import GraphQLError
 from django.db.models import Q
 from inventory.types import KegType, FillType, BatchType, BeerType
+from inventory.models import Keg, Fill, Batch, Beer
 from inventory.mutations import CreateKeg
 
 
 class Query(graphene.ObjectType):
     kegs = graphene.List(KegType)
+    keg = graphene.Field(KegType, tag=graphene.String())
     fills = graphene.List(FillType)
     batches = graphene.List(BatchType)
     beers = graphene.List(BeerType)
 
     def resolve_kegs(self, info, **kwargs):
         return Keg.objects.all()
+
+    def resolve_keg(self, info, tag):
+        return Keg.objects.get(tag=tag)
 
     def resolve_fills(self, info, **kwargs):
         return Fill.objects.all()
