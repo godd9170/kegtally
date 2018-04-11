f2cd5de056ae2e0fd0d6e795db819366ada23ad9
Henry Goddard <godd9170@mylaurier.ca>
2018-04-11 17:46:24 -0400

    get rid of connection type

--- inventory/types.py
+++ inventory/types.py
@@ -19,18 +19,7 @@ class BatchType(DjangoObjectType):
         interfaces = (graphene.relay.Node, )
 
 
-class CountableConnection(graphene.Connection):
-    total_count = graphene.Int()
-
-    class Meta:
-        node = BatchType
-
-    def resolve_total_count(self, info):
-        return self.iterable.count()
-
-
 class BeerType(DjangoObjectType):
     class Meta:
         model = Beer
         interfaces = (graphene.relay.Node, )
-        connection_class = CountableConnection
